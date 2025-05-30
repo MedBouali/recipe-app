0b39e786342d5bab892cd544efb438867cde7a45
Mohammed Bouali <68650187+MedBouali@users.noreply.github.com>
2025-05-30 02:21:31 +0100

    Add backend folder and basic Spring Boot setup

---
+++ recipe-app-backend/src/test/java/com/med/recipe_app_backend/RecipeAppBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.med.recipe_app_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipeAppBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
